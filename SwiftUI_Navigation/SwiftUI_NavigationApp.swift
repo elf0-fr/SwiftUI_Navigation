41328cf8eab61f5cf81bf1052f16dce57ac6eaf8
Romain Tirbisch <romain_tirbisch@yahoo.com>
2025-01-12 11:11:51 +0100

    Initial Commit

---
+++ SwiftUI_Navigation/SwiftUI_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NavigationApp.swift
+//  SwiftUI_Navigation
+//
+//  Created by Elfo on 12/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
